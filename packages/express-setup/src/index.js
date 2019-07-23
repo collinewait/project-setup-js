441af1ec168f26114649734379d8ac6427a6889c
collinewait <collinewait17@gmail.com>
2019-07-23 16:59:21 +0300

    get the port number from environment variables

--- packages/express-setup/src/index.js
+++ packages/express-setup/src/index.js
@@ -15,6 +15,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
 );
