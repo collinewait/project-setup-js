4d8452848162676f4516074e32fdbef41f2fbe9a
collinewait <collinewait17@gmail.com>
2019-07-23 16:43:06 +0300

    create a simple express server

--- packages/express-setup/src/index.js
+++ packages/express-setup/src/index.js
@@ -1,6 +1,15 @@
 import 'dotenv/config';
+import express from 'express';
+
 import y from './another';
 
 console.log('Hello Node.js project.');
 y()
 console.log(process.env.MY_SECRET);
+
+
+const app = express();
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
