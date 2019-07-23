0a118bd2f18fb3af161188d4736c830cae10c8ba
collinewait <collinewait17@gmail.com>
2019-07-23 16:45:29 +0300

    add a hello world route

--- packages/express-setup/src/index.js
+++ packages/express-setup/src/index.js
@@ -1,14 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 
-import y from './another';
+// import y from './another';
 
-console.log('Hello Node.js project.');
-y()
-console.log(process.env.MY_SECRET);
-
-
+// console.log('Hello Node.js project.');
+// y()
+// console.log(process.env.MY_SECRET);
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
