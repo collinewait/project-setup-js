fdec2c27b68f0d4addc8e7c1512913e78d334d1f
collinewait <collinewait17@gmail.com>
2019-07-24 11:46:45 +0300

    seed the database

--- packages/express-setup/src/index.js
+++ packages/express-setup/src/index.js
@@ -33,8 +33,43 @@ const port = process.env.PORT || 3000;
 
 const eraseDatabaseOnSync = true;
 
+const createUsersWithMessages = async () => {
+    await models.User.create(
+      {
+        username: 'rwieruch',
+        messages: [
+          {
+            text: 'Published the Road to learn React',
+          },
+        ],
+      },
+      {
+        include: [models.Message],
+      },
+    );
+
+    await models.User.create(
+        {
+          username: 'ddavids',
+          messages: [
+            {
+              text: 'Happy to release ...',
+            },
+            {
+              text: 'Published a complete ...',
+            },
+          ],
+        },
+        {
+          include: [models.Message],
+        },
+    );
+};
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
-  app.listen(port, () =>
+   if (eraseDatabaseOnSync) {
+        createUsersWithMessages();
+    }
+   app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`),
-  );
+   );
 });
