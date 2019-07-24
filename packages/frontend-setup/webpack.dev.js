8037d438d54288b7a44e34a4682f8f8d04825ab1
collinewait <collinewait17@gmail.com>
2019-07-24 21:35:14 +0300

    improve developer experience by evaluating source maps faster in development mode

--- packages/frontend-setup/webpack.dev.js
+++ packages/frontend-setup/webpack.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  devtool: 'source-map',
+  devtool: 'eval-source-map',
   entry: './src/index.js',
   module: {
       rules: [
