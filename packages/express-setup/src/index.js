719c8090e618f0d922f4c70345c82e7125184d43
collinewait <collinewait17@gmail.com>
2019-07-24 13:41:31 +0300

    let endpoints used database data

--- packages/express-setup/src/index.js
+++ packages/express-setup/src/index.js
@@ -14,8 +14,6 @@ import routes from './routes';
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/session', routes.session);
-app.use('/users', routes.user);
 app.use(cors());
 app.use(async (req, res, next) => {
     req.context = {
@@ -25,7 +23,8 @@ app.use(async (req, res, next) => {
     next();
 });
 app.use('/messages', routes.message);
-
+app.use('/users', routes.user);
+app.use('/session', routes.session);
 
 
 
--- packages/express-setup/src/routes/message.js
+++ packages/express-setup/src/routes/message.js
@@ -1,38 +1,34 @@
-import uuidv4 from 'uuid/v4';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-
-router.get('/:messageId', (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-router.post('/', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
-
-  req.context.models.messages[id] = message;
-
-  return res.send(message);
-});
-
-router.delete('/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
-
-  req.context.models.messages = otherMessages;
-
-  return res.send(message);
-});
-
+router.get('/', async (req, res) => {
+    const messages = await req.context.models.Message.findAll();
+    return res.send(messages);
+  });
+  
+  router.get('/:messageId', async (req, res) => {
+    const message = await req.context.models.Message.findByPk(
+      req.params.messageId,
+    );
+    return res.send(message);
+  });
+  
+  router.post('/', async (req, res) => {
+    const message = await req.context.models.Message.create({
+      text: req.body.text,
+      userId: req.context.me.id,
+    });
+  
+    return res.send(message);
+  });
+  
+  router.delete('/:messageId', async (req, res) => {
+    const result = await req.context.models.Message.destroy({
+      where: { id: req.params.messageId },
+    });
+  
+    return res.send(true);
+  });
+  
 export default router;
