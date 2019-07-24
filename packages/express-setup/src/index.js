f0f6544d03851328106c57ffd274d53ab705668a
collinewait <collinewait17@gmail.com>
2019-07-24 13:51:23 +0300

    use express functions instead of body parser

--- packages/express-setup/src/index.js
+++ packages/express-setup/src/index.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
 import models, { sequelize } from './models';
 import routes from './routes';
@@ -12,8 +11,8 @@ import routes from './routes';
 // y()
 // console.log(process.env.MY_SECRET);
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(async (req, res, next) => {
     req.context = {
