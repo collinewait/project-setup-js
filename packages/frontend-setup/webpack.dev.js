363fef4380de1e447e233423d557bbd8ff6e16ae
collinewait <collinewait17@gmail.com>
2019-07-24 21:31:09 +0300

    create separate webpack files for production and development

---
+++ packages/frontend-setup/webpack.dev.js
@@ -0,0 +1,40 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  rootMode: "upward"
+              }
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js']
+    },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack bundled JavaScript Project',
+      template: './public/index.html'
+    })
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
