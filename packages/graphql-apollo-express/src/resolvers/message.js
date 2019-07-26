6db80de422b5c27c9f82bd2a6790de7f12b98400
collinewait <collinewait17@gmail.com>
2019-07-26 19:01:51 +0300

    connect to postgres database

--- packages/graphql-apollo-express/src/resolvers/message.js
+++ packages/graphql-apollo-express/src/resolvers/message.js
@@ -1,47 +1,36 @@
-/* eslint-disable no-param-reassign */
-import uuidv4 from 'uuid/v4';
-
 export default {
   Query: {
-    messages: (parent, args, { models }) => {
-      return Object.values(models.messages);
+    messages: async (parent, args, { models }) => {
+      const messages = await models.Message.findAll();
+      return messages;
     },
-    message: (parent, { id }, { models }) => {
-      return models.messages[id];
+    message: async (parent, { id }, { models }) => {
+      const message = await models.Message.findByPk(id);
+      return message;
     },
   },
 
   Mutation: {
-    createMessage: (parent, { text }, { me, models }) => {
-      const id = uuidv4();
-      const message = {
-        id,
+    createMessage: async (parent, { text }, { me, models }) => {
+      const createdMessage = await models.Message.create({
         text,
         userId: me.id,
-      };
-
-      models.messages[id] = message;
-      models.users[me.id].messageIds.push(id);
-
-      return message;
+      });
+      return createdMessage;
     },
 
-    deleteMessage: (parent, { id }, { models }) => {
-      const { [id]: message, ...otherMessages } = models.messages;
-
-      if (!message) {
-        return false;
-      }
-
-      models.messages = otherMessages;
-
-      return true;
+    deleteMessage: async (parent, { id }, { models }) => {
+      const deletionResponse = await models.Message.destroy({
+        where: { id },
+      });
+      return deletionResponse;
     },
   },
 
   Message: {
-    user: (message, args, { models }) => {
-      return models.users[message.userId];
+    user: async (message, args, { models }) => {
+      const messageOwner = await models.User.findByPk(message.userId);
+      return messageOwner;
     },
   },
 };
--- packages/graphql-apollo-express/src/resolvers/user.js
+++ packages/graphql-apollo-express/src/resolvers/user.js
@@ -1,21 +1,27 @@
 export default {
   Query: {
-    users: (parent, args, { models }) => {
-      return Object.values(models.users);
+    users: async (parent, args, { models }) => {
+      const users = await models.User.findAll();
+      return users;
     },
-    user: (parent, { id }, { models }) => {
-      return models.users[id];
+    user: async (parent, { id }, { models }) => {
+      const user = await models.User.findByPk(id);
+      return user;
     },
-    me: (parent, args, { me }) => {
-      return me;
+    me: async (parent, args, { models, me }) => {
+      const myself = await models.User.findByPk(me.id);
+      return myself;
     },
   },
 
   User: {
-    messages: (user, args, { models }) => {
-      return Object.values(models.messages).filter(
-        message => message.userId === user.id,
-      );
+    messages: async (user, args, { models }) => {
+      const userMessages = await models.Message.findAll({
+        where: {
+          userId: user.id,
+        },
+      });
+      return userMessages;
     },
   },
 };
