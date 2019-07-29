860ea714f57b03c65ca0debe706f1230ba82a069
collinewait <collinewait17@gmail.com>
2019-07-29 14:18:27 +0300

    Revert dataloader caching.

--- packages/graphql-apollo-express/src/index.js
+++ packages/graphql-apollo-express/src/index.js
@@ -44,8 +44,6 @@ const batchUsers = async (keys, models_) => {
   return keys.map(key => users.find(user => user.id === key));
 };
 
-const userLoader = new DataLoader(keys => batchUsers(keys, models));
-
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
@@ -77,7 +75,7 @@ const server = new ApolloServer({
         me,
         secret: process.env.SECRET,
         loaders: {
-          user: userLoader,
+          user: new DataLoader(keys => batchUsers(keys, models)),
         },
       };
     }
